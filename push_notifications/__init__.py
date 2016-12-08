8a5f1e90020fed339b4c913dcc5749d6dc5a32f5
ashish kr <ashish.kumar@getmeashop.com>
2016-12-08 16:02:08 +0530

    [update]: bumped version, fixed url and download url

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -1,8 +1,8 @@
 
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 
 class NotificationError(Exception):
-	pass
+    pass
--- setup.py
+++ setup.py
@@ -6,38 +6,38 @@ from distutils.core import setup
 README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 CLASSIFIERS = [
-	"Development Status :: 5 - Production/Stable",
-	"Environment :: Web Environment",
-	"Framework :: Django",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 2.7",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.3",
-	"Programming Language :: Python :: 3.4",
-	"Topic :: Software Development :: Libraries :: Python Modules",
-	"Topic :: System :: Networking",
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Networking",
 ]
 
 
 import push_notifications
 
 setup(
-	name="django-push-notifications",
-	packages=[
-		"push_notifications",
-		"push_notifications/api",
-		"push_notifications/migrations",
-		"push_notifications/management",
-		"push_notifications/management/commands",
-	],
-	author=push_notifications.__author__,
-	author_email=push_notifications.__email__,
-	classifiers=CLASSIFIERS,
-	description="Send push notifications to mobile devices through GCM or APNS in Django.",
-	download_url="https://github.com/jleclanche/django-push-notifications/tarball/master",
-	long_description=README,
-	url="https://github.com/jleclanche/django-push-notifications",
-	version=push_notifications.__version__,
+    name="django-push-notifications",
+    packages=[
+        "push_notifications",
+        "push_notifications/api",
+        "push_notifications/migrations",
+        "push_notifications/management",
+        "push_notifications/management/commands",
+    ],
+    author=push_notifications.__author__,
+    author_email=push_notifications.__email__,
+    classifiers=CLASSIFIERS,
+    description="Send push notifications to mobile devices through GCM or APNS in Django.",
+    download_url="https://github.com/noodlebreak/django-push-notifications/tarball/gcustom",
+    long_description=README,
+    url="https://github.com/noodlebreak/django-push-notifications",
+    version=push_notifications.__version__,
 )
